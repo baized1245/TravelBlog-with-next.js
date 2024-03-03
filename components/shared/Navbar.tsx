7633aa9703ffbd25b20024e166573be2a9e704b6
unknown <you@example.com>
2024-03-03 14:21:10 +0600

    useMenuActive Custom React Hook

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -5,10 +5,30 @@ import Button from "../ui/Button";
 import Route from "../ui/Route";
 import { navLinks } from "@/constants";
 import MobileMenu from "./MobileMenu";
+import useMenuActive from "@/hooks/useMenuActive";
 import { useEffect, useState } from "react";
 import clsx from "clsx";
 
 const Navbar = () => {
+  const [isScrolling, setIsScrolling] = useState(false);
+  // const [openUserMenu, setOpenUserMenu] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollX > 0) {
+        setIsScrolling(true);
+      } else {
+        setIsScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <nav className="py-4 w-full">
       <div className="w-[95%] mx-auto max-w-[1450px] flex items-center justify-between pb-5 border-b border-gray-100">
@@ -18,11 +38,19 @@ const Navbar = () => {
           </Link>
         </div>
         <ul className="flex items-center justify-center gap-16 flex-2 max-md:hidden">
-          {navLinks.map((link, index) => (
-            <li key={index}>
-              <Route route={link.route} label={link.label} />
-            </li>
-          ))}
+          {navLinks.map((link, index) => {
+            const isActive = useMenuActive(link.route);
+
+            return (
+              <li key={index}>
+                <Route
+                  route={link.route}
+                  label={link.label}
+                  isActive={isActive}
+                />
+              </li>
+            );
+          })}
         </ul>
 
         <div className="flex gap-5 flex-1 justify-end max-md:hidden">
