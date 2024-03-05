728eea73a7a8e5b4f991ef2257c870b2a1d15eb1
unknown <you@example.com>
2024-03-05 15:02:36 +0600

    Blog Post Page Styling

--- app/(site)/blog/[id]/page.tsx
+++ app/(site)/blog/[id]/page.tsx
@@ -1,3 +1,11 @@
+import Tag from "@/components/ui/Tag";
+import Image from "next/image";
+import {
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+} from "react-icons/ai";
+
 interface searchParamsTypes {
   id: string;
   title: string;
@@ -5,7 +13,7 @@ interface searchParamsTypes {
   paragraph: string;
   featured: boolean;
   topPost: boolean;
-  tags: string;
+  tags: string[];
   authorImage: string;
   authorName: string;
   publishDate: string;
@@ -13,7 +21,57 @@ interface searchParamsTypes {
 
 const page = ({ searchParams }: { searchParams: searchParamsTypes }) => {
   const post = searchParams;
-  return <div>{post.title}</div>;
+  return (
+    <div className="w-[95%] mx-auto max-w-[1450px]">
+      <div className="w-full h-[400px] relative mb-5">
+        <Image
+          fill
+          alt="Image for blog"
+          src={post.image_path}
+          className="object-cover"
+        />
+      </div>
+      <Tag text={post.tags} />
+      <h2 className="text-4xl font-extrabold uppercase text-tertiary my-3">
+        {post.title}
+      </h2>
+
+      <div className="flex md:gap-20 gap-5 relative mt-2 md:flex-row flex-col">
+        <aside className="md:sticky md:top-3/4 md:h-screen">
+          <span className="uppercase text-2xl font-extrabold text-tertiary">
+            Share:
+          </span>
+          <div className="flex text-3xl gap-5 text-gray-400 mt-2 [&>*]:border">
+            <AiOutlineFacebook />
+            <AiOutlineInstagram />
+            <AiOutlineTwitter />
+          </div>
+        </aside>
+        <p className="text-xl">
+          {post.paragraph}
+          {post.paragraph}
+          {post.paragraph}
+          {post.paragraph}
+          {post.paragraph}
+        </p>
+
+        <div className="mt-5 flex gap-5 items-center">
+          <Image
+            src={post.authorImage}
+            width={500}
+            height={500}
+            alt={`Image of ${post.authorName}`}
+            className="rounded-full w-20 h-20 object-cover"
+          />
+          <div className="flex gap-1 flex-col">
+            <span>{post.authorName}</span>
+            <span>{post.publishDate}</span>
+          </div>
+        </div>
+        <article></article>
+      </div>
+    </div>
+  );
 };
 
 export default page;
