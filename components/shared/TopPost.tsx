8e53a023787b1dc2b44b924c4e245a6f0e9f245f
unknown <you@example.com>
2024-03-05 12:55:02 +0600

    Blog Card Component & Latest Post Component

---
+++ components/shared/TopPost.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+import Tag from "../ui/Tag";
+import Overlay from "../ui/Overlay";
+
+const TopPost = () => {
+  return <div>TopPost</div>;
+};
+
+export default TopPost;
